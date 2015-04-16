c7a475537296292b499139c2bf7568a4cb6e9fab
Doran <samanthadoran3@gmail.com>
2015-04-16 07:44:16 -0400

    Begin implementation of functions

--- Function.cpp
+++ Function.cpp
@@ -6,19 +6,21 @@
 
 Function::Function(string signature) {
     this->signature = signature;
+    children = {};
 }
 
 Expression *Function::evaluate() {
-    string s;
-    for (int i = 0; i < children.size(); ++i) {
-        if (i != children.size() - 1) {
-            ;
-        }
-        else {
+    string s = toString();
+    s += "(";
 
-        }
-    }
-    return nullptr;
+    for (int i = 0; i < children.size(); ++i)
+        if (i != children.size() - 1)
+            s += children[i]->evaluate()->toString() + ", ";
+        else
+            s += children[i]->evaluate()->toString();
+
+    s += ")";
+    return new Expression(s);
 }
 
 string Function::toString() const {
--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@
 #include <regex>
 #include "TreeNode.h"
 #include "Operator.h"
+#include "Function.h"
 
 using namespace std;
+TreeNode * derive(unordered_map<string, Operator>, unordered_map<string, Function>, string, TreeNode *);
 
 stack<TreeNode *> addNode(stack<TreeNode *> operand_stack, Operator o) {
     if (o.toString() == "(" || o.toString() == ")")
@@ -88,9 +90,28 @@ TreeNode *ShuntingYard(unordered_map<string, Operator> operators, string toParse
         return new Expression("0");
 }
 
-//Returns a treenode that represents the derivative of the supplied function wrt 'wrt'
-TreeNode *derive(unordered_map<string, Operator> operators, string wrt, TreeNode *root) {
-    //It's an operator or function
+TreeNode * deriveFunction(unordered_map<string, Operator> operators, unordered_map<string, Function> functions, string wrt, Function * root) {
+    /*if(root->toString() == "sin") {
+        return operators.find("*")->second.setOperands(
+                {functions.find("cos")->second.setArguments(root->children), derive(operators, functions, wrt, root->children[0])});
+    }
+    if(root->toString() == "log") {
+        Operator * divisionOperator = operators.find("/")->second.setOperands({new Expression("1"), root->children[0]});
+        Operator * multiplyOperator = operators.find("*")->second.setOperands({divisionOperator, derive(operators, functions, wrt, root->children[0])});
+        return multiplyOperator;
+    }*/
+
+    Function failSafe("D");
+    return failSafe.setArguments({root});
+}
+
+//Returns a TreeNode that represents the derivative of the supplied function wrt 'wrt'
+TreeNode *derive(unordered_map<string, Operator> operators, unordered_map<string, Function> functions, string wrt, TreeNode *root) {
+    Function *functionRoot = dynamic_cast<Function *>(root);
+    if(functionRoot != nullptr)
+        return deriveFunction(operators, functions, wrt, functionRoot);
+
+    //It's an operator
     if (!root->children.empty()) {
         Operator *opRoot = dynamic_cast<Operator *>(root);
         switch (opRoot->toString()[0]) {
@@ -99,7 +120,7 @@ TreeNode *derive(unordered_map<string, Operator> operators, string wrt, TreeNode
                 vector<TreeNode *> newChildren;
 
                 for (auto c: opRoot->children)
-                    newChildren.push_back(derive(operators, wrt, c));
+                    newChildren.push_back(derive(operators, functions, wrt, c));
 
                 return operators.find("+")->second.setOperands(newChildren);
             }
@@ -108,7 +129,7 @@ TreeNode *derive(unordered_map<string, Operator> operators, string wrt, TreeNode
                 vector<TreeNode *> newChildren;
 
                 for (auto c: opRoot->children)
-                    newChildren.push_back(derive(operators, wrt, c));
+                    newChildren.push_back(derive(operators, functions, wrt, c));
 
                 return operators.find("-")->second.setOperands(newChildren);
             }
@@ -120,29 +141,29 @@ TreeNode *derive(unordered_map<string, Operator> operators, string wrt, TreeNode
                     //Safely get a copy of this tree
                     vector<TreeNode *> copyOfChildren = ShuntingYard(operators,
                                                                      opRoot->evaluate()->toString())->children;
-                    vector<TreeNode *> toPassMult;
+                    vector<TreeNode *> toPassMultiply;
 
                     //Go through variable by variable in each of the terms and select which one to derive
                     for (int k = 0; k < opRoot->children.size(); ++k) {
                         if (k == j)
-                            toPassMult.push_back(derive(operators, wrt, copyOfChildren[k]));
+                            toPassMultiply.push_back(derive(operators, functions, wrt, copyOfChildren[k]));
                         else
-                            toPassMult.push_back(copyOfChildren[k]);
+                            toPassMultiply.push_back(copyOfChildren[k]);
                     }
-                    toPassAdd.push_back(operators.find("*")->second.setOperands(toPassMult));
+                    toPassAdd.push_back(operators.find("*")->second.setOperands(toPassMultiply));
                 }
                 Operator *addOp = operators.find("+")->second.setOperands(toPassAdd);
                 return addOp;
             }
             case '/': {
                 //D/dx(f(x)/g(x)) = (f'(x)g(x) - f(x)g'(x))/(g(x))^2
-                Operator *multOp1 = operators.find("*")->second.setOperands(
-                        {derive(operators, wrt, opRoot->children[0]), opRoot->children[1]});
+                Operator *multiplyOp1 = operators.find("*")->second.setOperands(
+                        {derive(operators, functions, wrt, opRoot->children[0]), opRoot->children[1]});
 
-                Operator *multOp2 = operators.find("*")->second.setOperands(
-                        {opRoot->children[0], derive(operators, wrt, opRoot->children[1])});
+                Operator *multiplyOp2 = operators.find("*")->second.setOperands(
+                        {opRoot->children[0], derive(operators, functions, wrt, opRoot->children[1])});
 
-                Operator *subOp = operators.find("-")->second.setOperands({multOp1, multOp2});
+                Operator *subOp = operators.find("-")->second.setOperands({multiplyOp1, multiplyOp2});
                 Operator *powOp = operators.find("^")->second.setOperands({opRoot->children[1], new Expression("2")});
                 Operator *divOp = operators.find("/")->second.setOperands({subOp, powOp});
                 return divOp;
@@ -150,7 +171,7 @@ TreeNode *derive(unordered_map<string, Operator> operators, string wrt, TreeNode
             case '^': {
                 //D/dx(b^c) = c * b^(c - 1) * D/dx(b)
                 TreeNode *base = opRoot->children[0];
-                TreeNode *derivedBase = derive(operators, wrt, base);
+                TreeNode *derivedBase = derive(operators, functions, wrt, base);
 
                 TreeNode *oldExponent = opRoot->children[1];
                 Operator *newExponent = operators.find("-")->second.setOperands(
@@ -160,8 +181,8 @@ TreeNode *derive(unordered_map<string, Operator> operators, string wrt, TreeNode
                 Operator *newPowerOp = operators.find("^")->second.setOperands(
                         {ShuntingYard(operators, base->evaluate()->toString()), newExponent});
 
-                Operator *multOp = operators.find("*")->second.setOperands({newCoefficient, newPowerOp, derivedBase});
-                return multOp;
+                Operator *multiplyOp = operators.find("*")->second.setOperands({newCoefficient, newPowerOp, derivedBase});
+                return multiplyOp;
             }
         }
     }
@@ -182,6 +203,7 @@ TreeNode *derive(unordered_map<string, Operator> operators, string wrt, TreeNode
 
 int main() {
     unordered_map<string, Operator> operators;
+    unordered_map<string, Function> functions;
 
     operators.emplace("+", Operator(2, "+", false));
     operators.emplace("-", Operator(2, "-", false));
@@ -189,18 +211,24 @@ int main() {
     operators.emplace("*", Operator(3, "*", false));
     operators.emplace("^", Operator(4, "^", true));
 
+    functions.emplace("sin", Function("sin"));
+    functions.emplace("cos", Function("cos"));
+    functions.emplace("tan", Function("tan"));
+    functions.emplace("log", Function("log"));
+
     TreeNode *rootOfEquation = ShuntingYard(operators, "1/x^2");
 
     //cout << "f(x) = " << rootOfEquation->evaluate()->toString() << endl;
 
-    TreeNode *dRootOfEquation = derive(operators, "x", rootOfEquation);
+    TreeNode *dRootOfEquation = derive(operators, functions, "x", rootOfEquation);
     cout << "f'(x) = " << ((dRootOfEquation == nullptr) ? "nullptr" : dRootOfEquation->evaluate()->toString()) << endl;
 
-    vector<string> tokenized = tokenize(dRootOfEquation->evaluate()->toString());
+    if(dRootOfEquation != nullptr) {
+        vector<string> tokenized = tokenize(dRootOfEquation->evaluate()->toString());
 
-    for(auto s: tokenized)
-        cout << s << endl;
-
+        for (auto s: tokenized)
+            cout << s << endl;
+    }
 
 
     return 0;
